ac13fb10effdc2269d32383ced6a1f3ad3c2ec18
jflorek <32914156+jflorek@users.noreply.github.com>
2017-10-18 22:13:57 -0600

    Added LinkedList
    Created LinkedList class & defined interface - methods unimplemented for now

---
+++ src/LinkedList.java
@@ -0,0 +1,56 @@
+import java.io.PrintStream;
+
+/**
+ * A linked list
+ * @author Jonathon
+ */
+public class LinkedList {
+	private ListElement head;
+	
+	/**
+	 * Initializes a new LinkedList instance
+	 */
+	public LinkedList()
+	{
+		
+	}
+	
+	/**
+	 * Adds a value to the LinkedList
+	 * @param value
+	 * value to add to list
+	 */
+	public void addElement(int value)
+	{
+		
+	}
+	
+	/**
+	 * Gets a value from the LinkedList
+	 * @param index index of the value to retrieve
+	 * @return the value at the specified index
+	 */
+	public int getElement(int index)
+	{
+		// default return value
+		return 0;	
+	}
+	
+	/**
+	 * Deletes a value from the LinkedList
+	 * @param index index of the value to remove
+	 */
+	public void deleteElement(int index)
+	{
+		
+	}
+	
+	/**
+	 * Prints the contents of the LinkedList
+	 * @param print PrintStream instance to print to
+	 */
+	public void printList(PrintStream print)
+	{
+		
+	}
+}
