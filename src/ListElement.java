23bdf1456f6223c0126ea0acb22fcbfd1c8d70de
jflorek <32914156+jflorek@users.noreply.github.com>
2017-10-18 22:03:28 -0600

    Added ListElement
    Added ListElement class representing a single element of a linked list

---
+++ src/ListElement.java
@@ -0,0 +1,14 @@
+/**
+ * An element of a linked list
+ * @author Jonathon
+ */
+public class ListElement {
+	private ListElement next;
+	private int value;
+	
+	public ListElement()
+	{
+		next = null;
+		value = 0;
+	}
+}
