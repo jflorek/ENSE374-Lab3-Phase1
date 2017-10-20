0712cd9b2dbfaf5ece0eaa7738e483e86a44bddd
jflorek <32914156+jflorek@users.noreply.github.com>
2017-10-19 20:49:05 -0600

    Added commands
    Added the various commands available to the user

--- src/Program.java
+++ src/Program.java
@@ -14,7 +14,36 @@ public class Program {
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println(in.nextLine());
+		boolean stop = false;
+		while(!stop)
+		{
+			System.out.println("Commands (case-insensitive):"
+					+ "\nadd [val1, val2, ... valn] : adds values to the list"
+					+ "\nget [index] : gets the value at the specified index, or 0 if no element at index"
+					+ "\ndelete [index] : deletes value at specified index if it exists"
+					+ "\nprint : prints contents of list from begin to end"
+					+ "\nexit : end the program");
+			switch(in.next().toLowerCase())
+			{
+			case "add":
+				System.out.println("Adds values");
+				break;
+			case "get":
+				System.out.println("Gets a value");
+				break;
+			case "delete":
+				System.out.println("Deletes a value");
+				break;
+			case "print":
+				System.out.println("Prints values");
+				break;
+			case "exit":
+				stop = true;
+				break;
+			default:
+				System.out.println("Invalid command");
+			}
+		}
 
 	}
 
