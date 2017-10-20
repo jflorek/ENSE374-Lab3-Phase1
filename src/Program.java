81381c3eb27fb706462f77e5d52737091f7beef6
jflorek <32914156+jflorek@users.noreply.github.com>
2017-10-19 20:37:15 -0600

    main method
    Added main method

---
+++ src/Program.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/**
+ * The main class for our program
+ * @author Jonathon
+ *
+ */
+public class Program {
+
+	/**
+	 * Entry method for our program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.nextLine());
+
+	}
+
+}
